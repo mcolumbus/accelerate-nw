3e03735157f39d6ade3c3929e93b19cbb2bf06f4
mcolumbus <mcolumbus@google.com>
2017-02-23 10:36:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php 
 $ip = $_SERVER['REMOTE_ADDR'];
+$headers = apache_request_headers();
+$xff = $headers['X-Forwarded-For'];
 // display it back
 echo "<h1>Accelerate Network Lab</h1>";
+echo "<h2>Real Client IP</h2>";
+echo "X-Forwarded-For Header : " . $xff;
 echo "<h2>Client IP</h2>";
-echo "Your IP address : " . $ip;
+echo "Client IP : " .$ip;
 echo "<h2>Hostname</h2>";
 echo "Server Hostname: " . php_uname("n");
 echo "<h2>Server Location</h2>";
